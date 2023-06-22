ddd15d95234647a118bbafe54650ef62a97dc769
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-06-21 22:01:37 -0300

    criação do hero

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/images/logo.svg'
+
+const Hero = () => (
+  <div>
+    <img src={logo} alt="eFood" />
+    <p>Viva experiências gastronômicas no conforto da sua casa</p>
+  </div>
+)
+
+export default Hero
