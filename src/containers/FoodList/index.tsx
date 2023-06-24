9bad1ee81eb950b08be26ec68c98c3558cb9a4e5
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-06-23 21:40:00 -0300

    alteração para uso da classe foodmodel

--- src/containers/FoodList/index.tsx
+++ src/containers/FoodList/index.tsx
@@ -1,41 +1,23 @@
 import { List } from './styles'
 
-import pizza from '../../assets/images/pizza.png'
-
 import Food from '../../components/Food'
 
-const FoodList = () => (
+import FoodModel from '../../models/FoodModel'
+
+type Props = {
+  foods: FoodModel[]
+}
+
+const FoodList = ({ foods }: Props) => (
   <List>
-    <Food
-      photo={pizza}
-      name="Pizza Marguerita"
-      description="A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!"
-    />
-    <Food
-      photo={pizza}
-      name="Pizza Marguerita"
-      description="A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!"
-    />
-    <Food
-      photo={pizza}
-      name="Pizza Marguerita"
-      description="A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!"
-    />
-    <Food
-      photo={pizza}
-      name="Pizza Marguerita"
-      description="A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!"
-    />
-    <Food
-      photo={pizza}
-      name="Pizza Marguerita"
-      description="A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!"
-    />
-    <Food
-      photo={pizza}
-      name="Pizza Marguerita"
-      description="A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!"
-    />
+    {foods.map((food) => (
+      <Food
+        key={food.id}
+        photo={food.photo}
+        name={food.name}
+        description={food.description}
+      />
+    ))}
   </List>
 )
 
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -3,6 +3,54 @@ import Presentation from '../../components/Presentation'
 import FoodList from '../../containers/FoodList'
 
 import italianFoodBanner from '../../assets/images/italian_food_banner.png'
+import pizza from '../../assets/images/pizza.png'
+
+import FoodModel from '../../models/FoodModel'
+
+const comidas: FoodModel[] = [
+  {
+    id: 1,
+    photo: pizza,
+    name: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+  },
+  {
+    id: 2,
+    photo: pizza,
+    name: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+  },
+  {
+    id: 3,
+    photo: pizza,
+    name: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+  },
+  {
+    id: 4,
+    photo: pizza,
+    name: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+  },
+  {
+    id: 5,
+    photo: pizza,
+    name: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+  },
+  {
+    id: 6,
+    photo: pizza,
+    name: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!'
+  }
+]
 
 const Profile = () => (
   <>
@@ -13,7 +61,7 @@ const Profile = () => (
       imageUrl={italianFoodBanner}
       restaurantName="La Dolce Vita Trattoria"
     />
-    <FoodList />
+    <FoodList foods={comidas} />
   </>
 )
 
